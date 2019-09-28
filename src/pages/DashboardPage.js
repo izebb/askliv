b8b7d77976e07078d2d61d9acc640d23cd8bfdd6
Gabriel Izebhigie <izebhigie@gmail.com>
2019-09-28 11:51:29 +0200

    Add tabs and content

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link, BrowserRouter as Router, Route,Switch } from 'react-router-dom'
 
-import Logo from '../assets/purple_logo.png'
+import Logo from '../assets/purple_logo.png';
+import Vaccination from '../assets/vaccination.svg';
 
 const height = 150
 
@@ -48,7 +49,25 @@ const StyledLink = styled(Link)`
   text-decoration: none;
   color: #444;
   font-size: 18px;
+  position: relative;
   padding: 20px 10px;
+
+  &:before {
+    height: 5px;
+    width: 100%;
+    content: ' ';
+    opacity: 0;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    background-color: #381a57;
+    border-radius: 5px 5px 0 0;
+    ${(({isActive}) => isActive && ({
+      opacity: 1
+    }))}
+  }
+
+  
 `
 
 const H1 = styled.h1`
@@ -58,7 +77,7 @@ const H1 = styled.h1`
 `
 const Content = styled.div`
   padding-top: ${height + 10}px;
-  max-width: 1000px;
+  max-width: 900px;
   margin: 20px auto;
   
 `
@@ -73,16 +92,20 @@ export const DashboardPage = () => {
 
         <Navigation>
           <List>
-            <StyledLink>Checklist</StyledLink>
+            <StyledLink isActive to="/dashboard/checklist">Checklist</StyledLink>
           </List>
           <List>
-            <StyledLink>Info</StyledLink>
+            <StyledLink to="/dashboard">Info</StyledLink>
           </List>
         </Navigation>
       </Topbar>
       <Content>
-        <H1>Get Ready for your trip to Malaysia</H1>
-        <Accordion />
+        <Router>
+          <Switch>
+            <Route exact path='/dashboard/checklist' component={Checklist} />
+            <Route exact path='/dashboard' component={Checklist} />
+            </Switch>
+        </Router>
       </Content>
     </>
   )
@@ -90,18 +113,29 @@ export const DashboardPage = () => {
 
 const AccordionHeader = styled.div`
   background-color: #f4f4f4;
-  padding: 20px 10px;
+  padding: 20px 30px;
   position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 5px 5px 0  0;
 `
 const AccordionHeaderTitle = styled.div`
-  padding: 20px 50px;
+  padding: 20px;
   font-size: 22px;
   font-weight: 500;
   color: #000;
   position: relative;
 `
-const AccordionBody = styled.div``
-const AccordionWrapper = styled.div``
+const AccordionBody = styled.div`
+    border-top: 2px solid #DDD;
+    font-size: 18px;
+    line-height: 1.5;
+    color: #333;
+`
+const AccordionWrapper = styled.div`
+    margin-bottom: 20px;
+`
 const No  = styled.div`
     position: absolute;
     pointer-events: none;
@@ -109,17 +143,58 @@ const No  = styled.div`
     font-weight: 900;
     top: -30px;
     left: -10px;
-    opacity: 0.3;
+    opacity: 0.2;
 `;
 
-const Accordion = () => {
+const VaccinationImg = styled.img`
+    width: 50px;
+    width: 110px;
+    left: -20px;
+    position: absolute;
+    bottom: -30px;
+    opacity: 0.12;
+`
+
+const Button = styled.button`
+  margin-left: auto;
+  background: #DDD;
+  border-radius: 1em;
+  font-size: 15px;
+  border: none;
+  padding: 5px 15px;
+  font-weight: 600;
+`;
+
+const Accordion = ({ title}) => {
   return (
     <AccordionWrapper>
       <AccordionHeader>
-        <No>1</No>
-        <AccordionHeaderTitle>Vaccination</AccordionHeaderTitle>
+        <VaccinationImg src={Vaccination} />
+        <AccordionHeaderTitle>{title}</AccordionHeaderTitle>
+        <Button>Mark as done</Button>
       </AccordionHeader>
-      <AccordionBody></AccordionBody>
+      <AccordionBody>
+
+    <p>
+        Est cupidatat exercitation ad eiusmod proident. Aute labore velit ea aliquip veniam est Lorem tempor tempor mollit. Cillum veniam reprehenderit cupidatat aliquip.
+    </p>
+          
+    <p>
+    Ad velit cillum pariatur ex cillum aliquip quis. Incididunt duis aliqua elit deserunt anim non non elit commodo nulla. Nisi pariatur est Lorem tempor mollit nisi proident aute exercitation consequat. Dolor non labore dolor eu aute esse irure magna fugiat. Cupidatat duis irure cillum adipisicing ea ullamco. Laboris proident nostrud minim consequat cillum.
+    </p>
+    <p>
+
+    Qui ullamco mollit enim enim commodo id. Laborum reprehenderit incididunt in consectetur irure deserunt voluptate labore duis. Velit Lorem nisi laboris amet sint laboris.
+    </p>
+      </AccordionBody>
     </AccordionWrapper>
   )
-}
+};
+
+const Checklist = () => (
+  <>
+  <H1>Get Ready for your trip to Malaysia ✈️</H1>
+          <Accordion  title="Vaccination"/>
+          <Accordion title="Pregnancy" />
+          </>
+)
